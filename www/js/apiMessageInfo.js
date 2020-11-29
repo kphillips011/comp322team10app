96865aec0c637305f29eb90c307ebc71f6a11513
pjack7oo <pjackowski@luc.edu>
2020-11-29 04:36:31 -0600

    not necessary

--- www/js/apiMessageInfo.js
+++
@@ -1,7 +0,0 @@
-var vinylBase = vinylBase || {};
-
-vinylBase.apiMessages = vinylBase.apiMessages || {};
-vinylBase.apiMessages.USERNAME_DOESNT_EXIST = 0;
-vinylBase.apiMessages.INVALID_PASSWORD = 1;
-vinylBase.apiMessages.USERNAME_ALREADY_EXISTS = 2;
-vinylBase.ApiMessages.COULD_NOT_CREATE_USER = 3;
