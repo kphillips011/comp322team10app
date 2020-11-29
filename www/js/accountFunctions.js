84ac19baec5e93fb31ac7d00d5e3e7450e761cf9
pjack7oo <pjackowski@luc.edu>
2020-11-29 04:37:32 -0600

    logout, sepreated queries, added album query

--- www/js/accountFunctions.js
+++ www/js/accountFunctions.js
@@ -2,69 +2,145 @@
 //will contain functions like delete, create,
 
 
-var userPlaylists;
+function logout() {
+    firebase.auth().signOut().then(() => {
+        alert("User Logged Out");
+        $.mobile.changePage($("#login_page"));
+    })
+}
+
+var userPlaylists,
+    userAlbums;
+
+$(document).ready(function() {
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            alert("grabbed userID")
+            window.localStorage.setItem("userID", user.uid);
+            $.mobile.changePage($("#homePage"));
+        }
+        else {
+            // User is signed out
+            // ...
+        }
+    });
+});
 
 
+
+function userPlaylistsQuery(UserID) {
+    var db = firebase.firestore();
+    //alert("loaded user" + uid);
+    var platlistsRef = db.collection("Playlist");
+    var userPlaylistsQuery = platlistsRef.where("UserID", "==", UserID);
+    return userPlaylistsQuery;
+}
+
+function userAlbumsQuery(UserID) {
+    //might be moved out so it doesnt need to be recalled ever query
+    var db = firebase.firestore();
+    
+    var AlbumsRef = db.collection("Album");
+    var userAlbumsQuery = AlbumsRef.where("UserID", "==", UserID);
+    return userAlbumsQuery;
+}
+
+function populateAlbums(UserID) {
+    var query = userAlbumsQuery(UserID);
+
+    var albumsDiv = document.getElementById("Albums");
+
+    query.get().then(function(querySnapshot) {
+        userAlbums = querySnapshot;
+        userAlbums.forEach(function (doc) {
+            var link = createAlbumLink(doc);
+
+            albumsDiv.appendChild(link);
+
+        }) 
+    })
+    .catch(function(error) {
+        console.log(error);
+    })
+}
+
+function createAlbumLink(album) {
+    var link = document.createElement("a");
+    
+    link.innerHTML = album.data().Name;
+    link.className = "textbutton";
+    link.setAttribute("uid", album.id);
+    link.setAttribute("isplaylist", false);
+    link.onclick = test;
+
+    return link;
+}
+
+
+function populatePlaylist(playlist) {
+    var playlistsDiv = document.getElementById("Playlists");
+    
+    var link = document.createElement('a');
+    // adding attributes to link
+    link.innerText = playlist.data().Name;
+    link.className = "textbutton playlistButton";
+    link.id = "playlistLink";
+    link.setAttribute("uid", playlist.id);
+    link.setAttribute("isplaylist", true);
+    link.onclick = test;
+    //console.log(link.href);
+    playlistsDiv.appendChild(link);
+}
 
 //this code will only run when the homePage is being loaded
 
 $(document).on("pagebeforeshow", "#homePage", function () {
     //alert('This page was just hidden: ');
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            if (jQuery.isEmptyObject(userPlaylists)) {
-                var uid = user.uid;
-                
-                var db = firebase.firestore();
-                //alert("loaded user" + uid);
-                var platlistsRef = db.collection("Playlist");
-                var userPlaylistsQuery = platlistsRef.where("UserID", "==", uid);
-                //alert(uid);
-                //alert(userPlaylists);
-                var playlistsDiv = document.getElementById("Playlists");
-                userPlaylistsQuery.get()
-                .then(function(querySnapshot) {
-                    userPlaylists = querySnapshot;
-                    userPlaylists.forEach(function(doc) {
-                        var playlist = doc.data();
-                        console.log(playlist.Songs[0].Name);
-                        var link = document.createElement('a');
-                        //link.href = "#albumPage?id="+doc.id;
-                        link.innerText = playlist.Name;
-                        link.className = "textbutton";
-                        link.id = "playlistLink";
-                        link.setAttribute("uid",doc.id);
-                        link.onclick = test;
-                        console.log(link.href);
-                        playlistsDiv.appendChild(link);
-                    });
-                })
-                .catch(function(error) {
-                    console.log( error);
+    //console.log(userPlaylists);
+    var uid = window.localStorage.getItem("userID");
+    if (userPlaylists == undefined) {
+        
+
+        var query = userPlaylistsQuery(uid);
+        //alert(uid);
+        //alert(userPlaylists);
+        
+        query.get()
+            .then(function (querySnapshot) {
+                userPlaylists = querySnapshot;
+                userPlaylists.forEach(function (doc) {
+                    //var playlist = doc.data();
+                    //console.log(playlist.Songs[0].Name);
+                    populatePlaylist(doc);
                 });
-                
-            }
-            else {
-                //not sure if necesary but i dont want the code above reruning every time
-                //might need code here if a playlist is added via add button/scan album
-            }
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
 
-            // ...
-        } else {
-            // User is signed out
-            // ...
-        }
+    }
+    else {
+        //not sure if necesary but i dont want the code above reruning every time
+        //might need code here if a playlist is added via add button/scan album
+    }
 
-    });
+    if (userAlbums == undefined) {
+        populateAlbums(uid);
+    }
 });
 
+// $(".playlistButton").on("click", function(e) {
+//     e.preventDefault();
+//     window.localStorage.setItem("uid", this.getAttribute("uid"));
+//     window.localStorage.setItem("isPlaylist", this.getAttribute("isplaylist"));
+//     $.mobile.changePage($("#albumPage"));
+// })
+
 function test() {
-    $(".textbutton").on("click", function() {
-        //alert($("#playlistLink").attr("uid"));
-        window.localStorage.setItem("uid", this.getAttribute("uid"));
-        $.mobile.changePage($("#albumPage"), {data:{id:this.uid}});
-        
-    });
+    //adding uid from element to local storage so the loaded page can grab it
+    //alert("test")
+    window.localStorage.setItem("uid", this.getAttribute("uid"));
+    window.localStorage.setItem("isPlaylist", this.getAttribute("isplaylist"));
+    $.mobile.changePage($("#albumPage"));
+    
 }
