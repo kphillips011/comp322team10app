cdd0a0998369333ff850efac917f999a05238f3e
pjack7oo <pjackowski@luc.edu>
2020-11-30 04:35:09 -0600

    added acnt function for email, pass, createplays

--- www/js/accountFunctions.js
+++ www/js/accountFunctions.js
@@ -22,6 +22,7 @@ $(document).ready(function() {
         else {
             // User is signed out
             // ...
+            $.mobile.changePage($("#login_page"));
         }
     });
 });
@@ -71,7 +72,7 @@ function createAlbumLink(album) {
     link.className = "textbutton";
     link.setAttribute("uid", album.id);
     link.setAttribute("isplaylist", false);
-    link.onclick = test;
+    link.onclick = generateOptionsPopup;
 
     return link;
 }
@@ -87,46 +88,51 @@ function populatePlaylist(playlist) {
     link.id = "playlistLink";
     link.setAttribute("uid", playlist.id);
     link.setAttribute("isplaylist", true);
-    link.onclick = test;
+    
+    link.onclick = generateOptionsPopup;
     //console.log(link.href);
     playlistsDiv.appendChild(link);
 }
 
 //this code will only run when the homePage is being loaded
 
-$(document).on("pagebeforeshow", "#homePage", function () {
+$(document).on("pageshow", "#homePage", function () {
     //alert('This page was just hidden: ');
     //console.log(userPlaylists);
-    var uid = window.localStorage.getItem("userID");
-    if (userPlaylists == undefined) {
-        
-
-        var query = userPlaylistsQuery(uid);
-        //alert(uid);
-        //alert(userPlaylists);
-        
-        query.get()
-            .then(function (querySnapshot) {
-                userPlaylists = querySnapshot;
-                userPlaylists.forEach(function (doc) {
-                    //var playlist = doc.data();
-                    //console.log(playlist.Songs[0].Name);
-                    populatePlaylist(doc);
+    //alert("loading home");
+    //var uid = window.localStorage.getItem("userID");
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (userPlaylists == undefined) {
+            //var user = firebase.auth().currentUser;
+    
+            var query = userPlaylistsQuery(user.uid);
+            //alert(uid);
+            //alert(userPlaylists);
+            
+            query.get()
+                .then(function (querySnapshot) {
+                    userPlaylists = querySnapshot;
+                    userPlaylists.forEach(function (doc) {
+                        //var playlist = doc.data();
+                        //console.log(playlist.Songs[0].Name);
+                        populatePlaylist(doc);
+                    });
+                })
+                .catch(function (error) {
+                    console.log(error);
                 });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-
-    }
-    else {
-        //not sure if necesary but i dont want the code above reruning every time
-        //might need code here if a playlist is added via add button/scan album
-    }
-
-    if (userAlbums == undefined) {
-        populateAlbums(uid);
-    }
+    
+        }
+        else {
+            //not sure if necesary but i dont want the code above reruning every time
+            //might need code here if a playlist is added via add button/scan album
+        }
+    
+        if (userAlbums == undefined) {
+            populateAlbums(user.uid);
+        }
+    })
+    backButton = false;
 });
 
 // $(".playlistButton").on("click", function(e) {
@@ -157,6 +163,7 @@ $(document).ready(function() {
             }).catch(function(error) {
                 alert(error);
                 if (error.code == 'auth/requires-recent-login') {
+                    //open reauthentication window
                     $("#reAuthPopup").popup( "option", "positionTo", "window" );
                     $("#reAuthPopup").popup("open");
                     //todo sumbit code for reauth
@@ -178,6 +185,177 @@ $(document).ready(function() {
     });
 });
 
-$(document).on("pageshow","#settingsPage", function() {
-
+$(document).ready(function() {
+    $("#setNewEmail").submit(function(e) {
+        e.preventDefault();
+        var newEmail = $("#newEmail").val().trim();
+        var confrimEmail = $("#confirmEmail").val().trim();
+        if(newEmail == confrimEmail) {
+            var user = firebase.auth().currentUser;
+            user.updateEmail(newEmail).then(function() {
+                alert("New Email Set");
+                $("#newEmailPopup").popup("close");
+                $("#emailLine").text("Email: "+ newEmail);
+            }).catch(function(error) {
+                alert(error);
+                if (error.code == 'auth/requires-recent-login') {
+                    //open reauthentication window
+                    $("setNewEmail").popup("close");
+                    $("#reAuthPopup").popup( "option", "positionTo", "window" );
+                    $("#reAuthPopup").popup("open");
+                    //todo sumbit code for reauth
+                }
+                $("#newEmail").val("");
+                $("#confirmEmail").val("");
+            })
+        }
+        else {
+            $("#newEmail").val("");
+            $("#confirmEmail").val("");
+            alert("Passwords dont match");
+        }
+        
+        
+        //$.mobile.changePage($("#settingsPage"));
+        
+        return true;
+    });
 });
+
+function cancleDelete() {
+    $("#deletePopup").popup("close");
+}
+
+function deleteUser() {
+    var user = firebase.auth().currentUser;
+
+    user.delete().then(function() {
+        alert("Account Deleted");
+        //go back to login
+        $.mobile.changePage($("#login_Page"));
+    }).catch(function(error) {
+        alert(error)
+        //reauth
+    });
+}
+
+$(document).on("pageshow","#settingsPage", function() {
+    var user = firebase.auth().currentUser;
+    
+    if(user) {
+        var email = user.email;
+        
+        $("#emailLine").text("Email: "+ email);
+    } else {
+
+    }
+});
+
+
+function generateOptionsPopup() {
+    let id = this.getAttribute("uid");
+    let isPlaylist = this.getAttribute("isplaylist");
+    let name = this.innerHTML;
+    
+    
+    $("#optionsTitle").text(name + " Options");
+    $("#optionsPopup").popup("open");
+
+    $("#viewItem").attr("uid",id);
+    $("#viewItem").attr("isplaylist",isPlaylist);
+    $("#viewItem").attr("name",name);
+
+    $("#deleteItem").attr("uid",id);
+    $("#deleteItem").attr("isplaylist",isPlaylist);
+    //$("#viewItem").attr("onclick",viewButton);
+
+}
+
+
+
+$(document).ready(function() {
+    $("#viewItem").on("click", function() {
+        backButton = false;
+        let id = this.getAttribute("uid");
+        let isPlaylist = this.getAttribute("isplaylist");
+        let name = this.getAttribute("name");
+        window.localStorage.setItem("uid", id);
+        window.localStorage.setItem("isPlaylist", isPlaylist);
+        window.localStorage.setItem("name", name);
+        $.mobile.changePage($("#albumPage"));
+    });
+});
+
+$(document).ready(function() {
+    $("#deleteItem").on("click", function() {
+        backButton = false;
+        let id = this.getAttribute("uid");
+        let isPlaylist = this.getAttribute("isplaylist");
+        
+        deleteItem(id, isPlaylist);
+        $("#optionsPopup").popup("close");
+    });
+})
+
+function deleteItem(id, isPlaylist) {
+    var db = firebase.firestore();
+    if(isPlaylist) {
+        db.collection("Playlist").doc(id).delete()
+        .then(function() {
+            alert("Playlist deleted");
+            deletePlaylist(id);
+        }).catch(function(error) {
+            alert(error);
+        });
+    }
+    else {
+        db.collection("Album").doc(id).delete()
+        .then(function() {
+            alert("Album deleted");
+
+        }).catch(function(error) {
+            alert(error);
+        });
+    }
+}
+
+function deletePlaylist(id) {
+    $(".playlistButton").each(function() {
+        if(this.getAttribute("uid")==id) {
+            this.remove();
+        }
+    })
+}
+
+function addPlaylist(name) {
+    var user = firebase.auth().currentUser;
+
+    var db = firebase.firestore();
+
+    db.collection("Playlist").add({
+        Name: name,
+        UserID: user.uid,
+        Songs:[]
+    }).then(function(doc) {
+        alert("Created "+ name);
+        db.collection("Playlist").doc(doc.id).get().then(function(playlist) {
+            populatePlaylist(playlist);
+        }).catch(function(error) {
+            alert(error);
+        });
+        
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
+$(document).ready(function() {
+    $("#createPlaylist").submit(function(e) {
+        e.preventDefault();
+        let name = $("#playlistName").val().trim();
+        addPlaylist(name);
+        $("#playlistName").val("");
+        $("#createPlaylistPopup").popup("close");
+    });
+});
+
