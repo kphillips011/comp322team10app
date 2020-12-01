8c14f75883545dfffe6ed19e4dcb79a1c8201ed0
thomasdimonte <tdimonte@luc.edu>
2020-12-01 16:05:47 -0600

    added apiscript.js

---
+++ www/js/apiscript.js
@@ -0,0 +1,18 @@
+// $.getJSON(
+// "https://api.discogs.com/artists/1?callback=callbackname", //ik you put the token or access key in here at the end somehow
+// function(data) {
+//     console.log(data);
+
+//     var name = data.name;
+
+//     $(".name").attr("src, name");
+// });
+
+const api_url = "https://api.discogs.com/artists/1?callback=callbackname";
+async function getAlbum() {
+    const response = await fetch(api_url);
+    const data = await resonse.json();
+    console.log(data);
+}
+
+getAlbum();
