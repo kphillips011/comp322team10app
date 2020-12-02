8edcbfed0f9aa662a45dfcbab882e498466730d1
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2020-12-01 18:34:44 -0600

    Write proper test code
    Files edited by Nicholas M. Synovic

--- www/js/apiscript.js
+++ www/js/apiscript.js
@@ -1,14 +1,18 @@
-var myHeaders = new Headers();
-myHeaders.append("User-Agent", " Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
-myHeaders.append("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY");
+function search(title) {
+    var myHeaders = new Headers();
+    myHeaders.append("User-Agent", " Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
+    myHeaders.append("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY");
 
-var requestOptions = {
-    method: 'GET',
-    headers: myHeaders,
-    redirect: 'follow'
-};
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
 
-fetch("https://api.discogs.com/database/search?title=nirvana - nevermind&per_page=100", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+    fetch("https://api.discogs.com/database/search?title=" + title + "&per_page=100", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+search("fall out boy")
