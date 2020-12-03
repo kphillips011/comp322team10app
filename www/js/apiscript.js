1b3e2b8087fe44d66992ad6f77b0efe8169de8bf
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2020-12-02 22:15:49 -0600

    Write code using XMLHTTPRequest instead of fetch
    Files edited by Nicholas M. Synovic

--- www/js/apiscript.js
+++ www/js/apiscript.js
@@ -1,6 +1,6 @@
 function search(title) {
     var myHeaders = new Headers();
-    myHeaders.append("User-Agent", " Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
+    myHeaders.append("User-Agent", "Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
     myHeaders.append("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY");
 
     var requestOptions = {
@@ -10,18 +10,46 @@ function search(title) {
     };
 
     var res = fetch("https://api.discogs.com/database/search?title=" + title + "&per_page=100", requestOptions)
-        .then(function (response) {
-            response.json()
-        })
+        .then(response => response.json())
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 
-    const json = res;
-    const obj = JSON.parse(json);
-    console.log(obj.title);
-    console.log(obj.cover_image);
-    console.log(obj.thumb);
+    // const json = res;
+    // const obj = JSON.parse(json);
+    // console.log(obj.title);
+    // console.log(obj.cover_image);
+    // console.log(obj.thumb);
 
 }
 
-search("fall out boy")
+
+program("https://api.discogs.com/database/search?title=" + title + "&per_page=100")
+
+var request = new XMLHttpRequest()
+
+function program(title) {
+    request.open("GET", "https://api.discogs.com/database/search?title=" + title + "&per_page=100", true)
+
+    request.setRequestHeader("User-Agent", "Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
+
+    request.setRequestHeader("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY")
+
+    request.onload = function () {
+        console.log(JSON.parse(this.response))
+    }
+    // request.onload = function () {
+    //     response = JSON.parse(this.response)
+    //     var next = nextPage(request.getResponseHeader("Link"))
+    //     if (next == null) {
+    //         return null
+    //     }
+    //     else {
+    //         program(next)
+    //         return null
+    //     }
+    // }
+    request.send()
+    return null
+}
+
+program("fall out boy")
