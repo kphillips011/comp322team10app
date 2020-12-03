e82b10a7c34a1eff46a010825b8018ef6586ea48
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2020-12-02 23:10:18 -0600

    Create a working searcher with Thomas
    Files edited by Nicholas M. Synovic

--- www/js/apiscript.js
+++ www/js/apiscript.js
@@ -1,55 +1,35 @@
-function search(title) {
-    var myHeaders = new Headers();
-    myHeaders.append("User-Agent", "Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
-    myHeaders.append("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY");
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    var res = fetch("https://api.discogs.com/database/search?title=" + title + "&per_page=100", requestOptions)
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
-
-    // const json = res;
-    // const obj = JSON.parse(json);
-    // console.log(obj.title);
-    // console.log(obj.cover_image);
-    // console.log(obj.thumb);
-
-}
-
-
-program("https://api.discogs.com/database/search?title=" + title + "&per_page=100")
-
 var request = new XMLHttpRequest()
 
-function program(title) {
-    request.open("GET", "https://api.discogs.com/database/search?title=" + title + "&per_page=100", true)
+function program(artist, page) {
+
+    request.open("GET", "https://api.discogs.com/database/search?artist=" + artist + "&page=" + page + "&per_page=100", true)
 
     request.setRequestHeader("User-Agent", "Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
 
     request.setRequestHeader("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY")
 
     request.onload = function () {
-        console.log(JSON.parse(this.response))
+        json = JSON.parse(this.response)
+
+        // Below prints the first 100 results for a search on the artists name
+        console.log(json)
+
+        // Below recursively accesses the extra pages for a search
+        var lastPage = json.pagination.urls.last
+        var nextPage = json.pagination.urls.next
+
+        if (lastPage !== undefined) {
+            var nextPageNumber = nextPage.match("page=([0-9]+)")[1]
+            var lastPageNumber = lastPage.match("page=([0-9]+)")[1]
+            console.log(nextPageNumber)
+            if (page <= lastPageNumber) {
+                program(artist, nextPageNumber)
+            }
+        }
+
     }
-    // request.onload = function () {
-    //     response = JSON.parse(this.response)
-    //     var next = nextPage(request.getResponseHeader("Link"))
-    //     if (next == null) {
-    //         return null
-    //     }
-    //     else {
-    //         program(next)
-    //         return null
-    //     }
-    // }
     request.send()
     return null
 }
 
-program("fall out boy")
+program("fall out boy", 1)
