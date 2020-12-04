578cf452e20c28f8f34e93dac6eb047e84784664
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2020-12-03 21:40:01 -0600

    Write code to present song list
    Files edited by Nicholas M. Synovic

--- www/js/apiscript.js
+++ www/js/apiscript.js
@@ -1,15 +1,13 @@
 var request = new XMLHttpRequest()
 
-function program(page) {
-    console.log("PENIS")
-
+function generalSearch(page) {
     // Get the input from the search bar
     let input = document.getElementById('searchBar').value
     input = input.replace(/\s+/g, '-').toLowerCase();
 
     // Create the request with the proper uri and headers
     request.open("GET", "https://api.discogs.com/database/search?title=" + input + "&page=" + page + "&per_page=100", true)
-    request.setRequestHeader("User-Agent", "Comp322Project/1.0 +https://github.com/kphillips011/comp322team10app");
+    request.setRequestHeader("User-Agent", "VinylBase");
     request.setRequestHeader("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY")
 
     // Once there is a response from the server...
@@ -38,6 +36,8 @@ function program(page) {
                 const container = document.createElement("div")
                 container.setAttribute("class", "result-container")
 
+                const link = document.createElement("a")
+
                 const result = document.createElement("div")
                 result.setAttribute("id", "result")
 
@@ -55,13 +55,22 @@ function program(page) {
                 if (element.type !== "artist") {
                     var splitHere = element.title.indexOf(" - ")
 
+                    const artist = document.createElement("div")
+                    artist.setAttribute("id", "result-artist")
+                    artist.textContent = element.title.slice(0, splitHere)
+
                     const alublm = document.createElement("div")
                     alublm.setAttribute("id", "result-alublm")
                     alublm.textContent = element.title.slice(splitHere + 3)
 
-                    const artist = document.createElement("div")
-                    artist.setAttribute("id", "result-artist")
-                    artist.textContent = element.title.slice(0, splitHere)
+                    var linkArgs = "albulmParse('" + element.master_url + "', '" + element.thumb + "', '" + element.title.slice(0, splitHere) + "', '" + element.title.slice(splitHere + 3) + "')"
+
+                    if (element.master_url == null) {
+                        link.setAttribute("style", "color: black;")
+                    }
+                    else {
+                        link.setAttribute("onclick", linkArgs)
+                    }
 
                     text.appendChild(artist)
                     text.appendChild(alublm)
@@ -72,6 +81,8 @@ function program(page) {
                     artist.setAttribute("id", "result-artist")
                     artist.textContent = element.title
 
+                    link.setAttribute("style", "color: black;")
+
                     text.appendChild(artist)
                 }
 
@@ -80,7 +91,9 @@ function program(page) {
                 result.appendChild(text)
                 result.appendChild(photoContainer)
 
-                container.appendChild(result)
+                link.appendChild(result)
+
+                container.appendChild(link)
 
                 app.appendChild(container)
             }
@@ -91,7 +104,7 @@ function program(page) {
             var nextPageNumber = nextPage.match("page=([0-9]+)")[1]
             var lastPageNumber = lastPage.match("page=([0-9]+)")[1]
             if (page <= lastPageNumber) {
-                program(nextPageNumber)
+                generalSearch(nextPageNumber)
             }
         }
     }
@@ -99,3 +112,75 @@ function program(page) {
     request.send()
     return null
 }
+
+function albulmParse(url, imageURL, artist, albulm) {
+    request.open("GET", url, true)
+    request.setRequestHeader("User-Agent", "VinylBase");
+    request.setRequestHeader("Authorization", "Discogs key=JzKHUoDrREtQgpMdkEdu, secret=gQCBQrBCupEnpDwdAmLkxlJhOGZiwidY")
+
+    // Once there is a response from the server...
+    request.onload = function () {
+
+        // Turn the response into JSON
+        json = JSON.parse(this.response)
+
+        // Pull specific data from the JSON
+        var tracklist = json.tracklist
+
+        // Root HTML
+        const app = document.getElementById("results")
+
+        // Clear all child HTML
+        app.innerHTML = ""
+
+        // Creates  content from the response
+        tracklist.forEach(element => {
+
+            const container = document.createElement("div")
+            container.setAttribute("class", "result-container")
+
+            const result = document.createElement("div")
+            result.setAttribute("id", "result")
+
+            const photoContainer = document.createElement("div")
+            photoContainer.setAttribute("id", "result-photo")
+
+            const photo = document.createElement("img")
+            photo.setAttribute("src", imageURL)
+            photo.setAttribute("style", "display: block; margin-left: auto; margin-right: auto; ")
+
+            const text = document.createElement("div")
+            text.setAttribute("id", "result-text")
+
+            // Create elements for tracks
+            const artist_elem = document.createElement("div")
+            artist_elem.setAttribute("id", "result-artist")
+            artist_elem.textContent = artist
+
+            const alublm = document.createElement("div")
+            alublm.setAttribute("id", "result-alublm")
+            alublm.textContent = albulm
+
+            const song = document.createElement("div")
+            song.setAttribute("id", "result-song")
+            song.textContent = element.title
+
+            text.appendChild(artist_elem)
+            text.appendChild(alublm)
+            text.appendChild(song)
+
+            photoContainer.appendChild(photo)
+
+            result.appendChild(text)
+            result.appendChild(photoContainer)
+
+            container.appendChild(result)
+
+            app.appendChild(container)
+        });
+    }
+
+    request.send()
+    return null
+}
+function artistParse(url) { return null }
